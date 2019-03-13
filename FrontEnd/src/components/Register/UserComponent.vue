f2243fda7498e6a63ddd167e148ad0d08fb1882e
david <davidcamposch@gmail.com>
2019-03-13 01:03:34 -0600

    Register students view

--- FrontEnd/src/components/Register/UserComponent.vue
+++ FrontEnd/src/components/Register/UserComponent.vue
@@ -1,23 +1,59 @@
 <template>
-    <div id="user">
-        
-    </div>
+  <v-layout wrap mx-5>
+    <v-flex xs12 pt-2>
+      <v-text-field
+        xs12
+        v-model="user.email"
+        :rules="[rules.required, rules.min]"
+        label="E-mail"
+        required
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs12 pt-2>
+      <v-text-field
+        v-model="user.name"
+        :rules="[rules.required, rules.min]"
+        :counter="10"
+        label="Usuario"
+        required
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs12 pt-2>
+      <v-text-field
+        v-model="user.password"
+        :append-icon="show ? 'visibility' : 'visibility_off'"
+        :rules="[rules.required, rules.min]"
+        :type="show ? 'text' : 'password'"
+        name="input-10-1"
+        label="Ingrese su Contraseña"
+        hint="Min 8 Caracteres"
+        counter
+        @click:append="show = !show"
+      ></v-text-field>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-
 export default {
-    name:'user',
-    data(){
-        return {
-        }
-    },
-    methods:{
-
-    }
-}
+  name: "user",
+  data() {
+    return {
+      show: false,
+      user: {
+        password: "",
+        name: "",
+        email: ""
+      },
+      rules: {
+        required: value => !!value || "Requerido.",
+        min: v => v.length >= 8 || "Min 8 Caracteres"
+      }
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style scoped>
-
 </style>
--- FrontEnd/src/views/RegisterEstudents.vue
+++ FrontEnd/src/views/RegisterEstudents.vue
@@ -1,31 +1,42 @@
 <template>
   <div id="appregister">
     <v-layout justify-center mt-5>
-      <v-flex sm12>
-        <UserComponent :class="{'d-none': nextindex>1}"></UserComponent>
-        <PersonComponent :class="{'d-none': nextindex!==2}"></PersonComponent>
-        <div class="nooo" :class="{'d-none': nextindex<3}"></div>
-        <div>
-          <v-btn
-            color="blue"
-            block
-            flat
-            dark
-            round
-            @click="next"
-            :class="{'d-none': nextindex>2}"
-          >Siguiente</v-btn>
-          <v-btn
-            color="red"
-            block
-            flat
-            dark
-            round
-            @click="back"
-            :class="{'d-none': nextindex<2}"
-          >Atras</v-btn>
-        </div>
+      <v-flex xs12 md6 offset-md3>
+        <v-card>
+          <v-progress-linear color="success" height="5" :value="nextindex * 33.3"></v-progress-linear>
+          <v-img class="black--text" height="200px" :src="image">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12>
+                  <span class="headline font-italic" text-xs-center>Datos Estudiante</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+
+          <div>
+            <UserComponent :class="{'d-none': nextindex>1}"></UserComponent>
+            <PersonComponent :class="{'d-none': nextindex!==2}"></PersonComponent>
+            <div :class="{'d-none': nextindex<3}">
+              <v-layout wrap mx-5>
+                <v-flex xs12 d-flex>
+                  <v-select :items="grado" label="Grado"></v-select>
+                </v-flex>
+                <v-flex xs12 d-flex>
+                  <v-select :items="adecuacion" label="Adecuaciòn"></v-select>
+                </v-flex>
+              </v-layout>
+            </div>
+          </div>
+
+          <v-card-actions>
+            <v-btn round dark flat color="green" block @click="next" :class="registrar">Registarce</v-btn>
+            <v-btn round dark flat color="blue" block @click="next" :class="ocultarNext">Siguiente</v-btn>
+            <v-btn round block dark flat color="red" @click="back" :class="ocultarback">Atras</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
+
       <v-flex></v-flex>
     </v-layout>
   </div>
@@ -43,15 +54,27 @@ export default {
   },
   data() {
     return {
-      nextindex: 1
+      nextindex: 1,
+      ocultarNext: "no",
+      ocultarback: "si",
+      registrar: "si",
+      grado: ["Primero", "Segundo", "Tercero", "Cuarto", "Quinto"],
+      adecuacion: ["Ninguna", "Moderada", "Significativa"],
+      image: require("@/assets/img/studens1.jpg")
     };
   },
   methods: {
     next() {
       this.nextindex++;
+      this.ocultarNext = this.nextindex > 2 ? "si" : "no";
+      this.ocultarback = this.nextindex < 2 ? "si" : "no";
+      this.registrar = this.nextindex === 3 ? "no" : "si";
     },
     back() {
       this.nextindex--;
+      this.ocultarNext = this.nextindex > 2 ? "si" : "no";
+      this.ocultarback = this.nextindex < 2 ? "si" : "no";
+      this.registrar = this.nextindex === 3 ? "no" : "si";
     }
   }
 };
@@ -61,5 +84,14 @@ export default {
 .nooo {
   height: 40px;
   background-color: black;
+}
+.no {
+  display: block;
+}
+.si {
+  display: none;
+}
+.headline {
+  font-size: 20px !important;
 }
 </style>
