c5ea34b6b9aff6ed62143d81c1b897dbada578f9
Aldo <aldosteve@hotmail.com>
2019-03-19 23:41:58 -0600

    Creando Login

---
+++ Backend/resources/views/components/main.blade.php
@@ -0,0 +1,83 @@
+<div class="main">
+        <section class="acerca-de">
+            <div class="contenedor">
+                <h2 class="sobre-nosotros">Sobre nosotros</h2>
+                <h3 class="slogan">Tirando código en la web</h3>
+                <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dicta rem hic totam necessitatibus
+                    tempora animi error perferendis. Vero corrupti porro quia! Ad saepe alias, officiis voluptatem sed, odio
+                    dolores neque dolorem placeat nam quia numquam soluta ipsam nostrum consequuntur, a magnam non. Tenetur
+                    repudiandae distinctio inventore voluptate fugit laborum?</p>
+                <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dicta rem hic totam necessitatibus
+                    tempora animi error perferendis. Vero corrupti porro quia! Ad saepe alias, officiis voluptatem sed, odio
+                    dolores neque dolorem placeat nam quia numquam soluta ipsam nostrum consequuntur, a magnam non. Tenetur
+                    repudiandae distinctio inventore voluptate fugit laborum?</p>
+            </div>
+        </section>
+        <section class="galeria">
+            <div class="sesgoarriba"></div>
+            <div class="imagenes none">
+                <img src="/images/1.jpg" alt="">
+            </div>
+            <div class="imagenes">
+                <img src="/images/3.jpg" alt="">
+            </div>
+            <div class="imagenes">
+                <img src="/images/2.jpg" alt="">
+                <div class="encima">
+                    <h2>Herramientas Escolares</h2>
+                    <div></div>
+                </div>
+            </div>
+            <div class="imagenes">
+                <img src="/images/4.jpeg" alt="">
+    
+            </div>
+            <div class="imagenes none">
+                <img src="images/5.jpeg" alt="">
+            </div>
+            <div class="sesgoabajo"></div>
+        </section>
+        <section class="miembros">
+            <div class="contenedor">
+                <h2 class="sobre-nosotros">Nuestros Servicios</h2>
+                <h3 class="slogan">Conoce algunas utilidades del campus</h3>
+                <div class="cards">
+                    <div class="card">
+                        <img src="/images/11.jpg" alt="">
+                        <h4>Videos</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/11.jpg" alt="">
+                        <h4>Tareas</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                    <div class="card">
+                        <img src="/images/11.jpg" alt="">
+                        <h4>Noticias</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fondo">
+            <div class="sesgoarriba"></div>
+            <div class="contenedor">
+                <h2 class="titulo-patrocinadores">No se que poner</h2>
+                <h3 class="subtitulo-patrocinadores">Lorem ipsum dolor sit</h3>
+                <div class="clientes">
+                    <div class="cliente">
+                        <img src="/images/6.jpeg" alt="">
+                    </div>
+                    <div class="cliente">
+                        <img src="/images/6.jpeg" alt="">
+                    </div>
+                    <div class="cliente">
+                        <img src="/images/6.jpeg" alt="">
+                    </div>
+                </div>
+                <h3 class="subtitulo-patrocinadores especial">y Lorem ipsum dolor sit...</h3>
+            </div>
+            <div class="sesgoabajo-unico"></div>
+        </section>
+    </div>
