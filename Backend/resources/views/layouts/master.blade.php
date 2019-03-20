18b361e721372064b7dd311f674cb76eec7825d6
Aldo <aldosteve@hotmail.com>
2019-03-19 21:57:37 -0600

    configurando paquetes .json y .vendor

--- Backend/resources/views/layouts/master.blade.php
+++ Backend/resources/views/layouts/master.blade.php
@@ -54,6 +54,6 @@
         <!-- scripts -->
         @yield('js')
         <script src="{{ URL::asset('js/jquery-3.3.1.min.js') }}"></script>
-        <!--<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>-->
+      
     </body>
 </html>
