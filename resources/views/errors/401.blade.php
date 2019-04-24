0190beb2b308e78e67b5a69b9259f4466b84b7b2
dabakura <davidcamposch@gmail.com>
2019-04-23 19:53:42 -0600

    Limpiando para unificar

--- resources/views/errors/401.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>Esta accion no esta siemdo autorizada</h1>
-@endsection
--- resources/views/homeadministrador.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-@if (!Auth::guest())
-   <administrador name={{ Auth::user()->name }}></administrador>
-@endif
-@endsection
