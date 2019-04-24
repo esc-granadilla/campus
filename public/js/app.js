723aac66c5da2b9a8df55a56f89078288cb31f9b
dabakura <davidcamposch@gmail.com>
2019-04-24 03:58:02 -0600

    vista profesor beta

--- public/js/app.js
+++ public/js/app.js
@@ -6505,6 +6505,122 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/js/views/Profesor.vue?vue&type=script&lang=js&":
+/*!*********************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/assets/js/views/Profesor.vue?vue&type=script&lang=js& ***!
+  \*********************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      rowsPerPageItems: [4, 8, 12],
+      pagination: {
+        rowsPerPage: 4
+      },
+      items: [{
+        name: "Ciencias",
+        calories: 159,
+        fat: 6.0,
+        carbs: 24
+      }, {
+        name: "Matematicas",
+        calories: 237,
+        fat: 9.0,
+        carbs: 37
+      }, {
+        name: "Español",
+        calories: 262,
+        fat: 16.0,
+        carbs: 23
+      }, {
+        name: "Ingles",
+        calories: 305,
+        fat: 3.7,
+        carbs: 67
+      }, {
+        name: "Estudios Sociales",
+        calories: 356,
+        fat: 16.0,
+        carbs: 49
+      }, {
+        name: "Jelly bean",
+        calories: 375,
+        fat: 0.0,
+        carbs: 94
+      }, {
+        name: "Lollipop",
+        calories: 392,
+        fat: 0.2,
+        carbs: 98
+      }, {
+        name: "Honeycomb",
+        calories: 408,
+        fat: 3.2,
+        carbs: 87
+      }, {
+        name: "Donut",
+        calories: 452,
+        fat: 25.0,
+        carbs: 51
+      }, {
+        name: "KitKat",
+        calories: 518,
+        fat: 26.0,
+        carbs: 65
+      }]
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/js/views/RegisterProfesor.vue?vue&type=script&lang=js&":
 /*!*****************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/assets/js/views/RegisterProfesor.vue?vue&type=script&lang=js& ***!
@@ -44779,6 +44895,153 @@ var staticRenderFns = [
     ])
   }
 ]
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/js/views/Profesor.vue?vue&type=template&id=51d661bd&":
+/*!*************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/assets/js/views/Profesor.vue?vue&type=template&id=51d661bd& ***!
+  \*************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "v-container",
+    { attrs: { "grid-list-md": "" } },
+    [
+      _c("v-data-iterator", {
+        attrs: {
+          items: _vm.items,
+          "rows-per-page-items": _vm.rowsPerPageItems,
+          pagination: _vm.pagination,
+          "content-tag": "v-layout",
+          row: "",
+          wrap: ""
+        },
+        on: {
+          "update:pagination": function($event) {
+            _vm.pagination = $event
+          }
+        },
+        scopedSlots: _vm._u([
+          {
+            key: "item",
+            fn: function(props) {
+              return [
+                _c(
+                  "v-flex",
+                  { attrs: { xs12: "", sm6: "", md4: "", lg3: "" } },
+                  [
+                    _c(
+                      "v-card",
+                      [
+                        _c("v-card-title", [
+                          _c("h4", [_vm._v(_vm._s(props.item.name))])
+                        ]),
+                        _vm._v(" "),
+                        _c("v-divider"),
+                        _vm._v(" "),
+                        _c(
+                          "v-list",
+                          { attrs: { dense: "" } },
+                          [
+                            _c(
+                              "v-list-tile",
+                              [
+                                _c("v-list-tile-content", [_vm._v("Grado:")]),
+                                _vm._v(" "),
+                                _c(
+                                  "v-list-tile-content",
+                                  { staticClass: "align-end" },
+                                  [_vm._v(_vm._s(props.item.calories))]
+                                )
+                              ],
+                              1
+                            ),
+                            _vm._v(" "),
+                            _c(
+                              "v-list-tile",
+                              [
+                                _c("v-list-tile-content", [_vm._v("Dia:")]),
+                                _vm._v(" "),
+                                _c(
+                                  "v-list-tile-content",
+                                  { staticClass: "align-end" },
+                                  [_vm._v(_vm._s(props.item.fat))]
+                                )
+                              ],
+                              1
+                            ),
+                            _vm._v(" "),
+                            _c(
+                              "v-list-tile",
+                              [
+                                _c("v-list-tile-content", [_vm._v("Horario:")]),
+                                _vm._v(" "),
+                                _c(
+                                  "v-list-tile-content",
+                                  { staticClass: "align-end" },
+                                  [_vm._v(_vm._s(props.item.carbs))]
+                                )
+                              ],
+                              1
+                            ),
+                            _vm._v(" "),
+                            _c(
+                              "v-list-tile",
+                              [
+                                _c(
+                                  "v-list-tile-content",
+                                  { staticClass: "align-center" },
+                                  [
+                                    _c(
+                                      "v-btn",
+                                      {
+                                        attrs: {
+                                          round: "",
+                                          dark: "",
+                                          color: "primary",
+                                          block: ""
+                                        }
+                                      },
+                                      [_vm._v("Seleccionar")]
+                                    )
+                                  ],
+                                  1
+                                )
+                              ],
+                              1
+                            )
+                          ],
+                          1
+                        )
+                      ],
+                      1
+                    )
+                  ],
+                  1
+                )
+              ]
+            }
+          }
+        ])
+      })
+    ],
+    1
+  )
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -86197,6 +86460,7 @@ Vue.component('registerstudents', __webpack_require__(/*! ./views/RegisterStuden
 Vue.component('registerprofesor', __webpack_require__(/*! ./views/RegisterProfesor.vue */ "./resources/assets/js/views/RegisterProfesor.vue").default);
 Vue.component('remembercomponent', __webpack_require__(/*! ./components/Login/RememberComponent.vue */ "./resources/assets/js/components/Login/RememberComponent.vue").default);
 Vue.component('administrador', __webpack_require__(/*! ./views/Administrador.vue */ "./resources/assets/js/views/Administrador.vue").default);
+Vue.component('profesor', __webpack_require__(/*! ./views/Profesor.vue */ "./resources/assets/js/views/Profesor.vue").default);
 Vue.component('pmostrarcomponent', __webpack_require__(/*! ./components/Profesores/MostrarComponent.vue */ "./resources/assets/js/components/Profesores/MostrarComponent.vue").default);
 Vue.component('peditarcomponent', __webpack_require__(/*! ./components/Profesores/EditarComponent.vue */ "./resources/assets/js/components/Profesores/EditarComponent.vue").default);
 Vue.component('pborrarcomponent', __webpack_require__(/*! ./components/Profesores/BorrarComponent.vue */ "./resources/assets/js/components/Profesores/BorrarComponent.vue").default);
@@ -88592,6 +88856,75 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Login_vue_vue_type_template_id_33212926_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"]; });
 
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Login_vue_vue_type_template_id_33212926_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ }),
+
+/***/ "./resources/assets/js/views/Profesor.vue":
+/*!************************************************!*\
+  !*** ./resources/assets/js/views/Profesor.vue ***!
+  \************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Profesor_vue_vue_type_template_id_51d661bd___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Profesor.vue?vue&type=template&id=51d661bd& */ "./resources/assets/js/views/Profesor.vue?vue&type=template&id=51d661bd&");
+/* harmony import */ var _Profesor_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Profesor.vue?vue&type=script&lang=js& */ "./resources/assets/js/views/Profesor.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Profesor_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _Profesor_vue_vue_type_template_id_51d661bd___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _Profesor_vue_vue_type_template_id_51d661bd___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/assets/js/views/Profesor.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/assets/js/views/Profesor.vue?vue&type=script&lang=js&":
+/*!*************************************************************************!*\
+  !*** ./resources/assets/js/views/Profesor.vue?vue&type=script&lang=js& ***!
+  \*************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Profesor_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./Profesor.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/js/views/Profesor.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Profesor_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/assets/js/views/Profesor.vue?vue&type=template&id=51d661bd&":
+/*!*******************************************************************************!*\
+  !*** ./resources/assets/js/views/Profesor.vue?vue&type=template&id=51d661bd& ***!
+  \*******************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Profesor_vue_vue_type_template_id_51d661bd___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./Profesor.vue?vue&type=template&id=51d661bd& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/assets/js/views/Profesor.vue?vue&type=template&id=51d661bd&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Profesor_vue_vue_type_template_id_51d661bd___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Profesor_vue_vue_type_template_id_51d661bd___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
 
---
+++ resources/assets/js/views/Profesor.vue
@@ -0,0 +1,115 @@
+<template>
+   <v-container grid-list-md>
+      <v-data-iterator
+         :items="items"
+         :rows-per-page-items="rowsPerPageItems"
+         :pagination.sync="pagination"
+         content-tag="v-layout"
+         row
+         wrap
+      >
+         <template v-slot:item="props">
+            <v-flex xs12 sm6 md4 lg3>
+               <v-card>
+                  <v-card-title>
+                     <h4>{{ props.item.name }}</h4>
+                  </v-card-title>
+                  <v-divider></v-divider>
+                  <v-list dense>
+                     <v-list-tile>
+                        <v-list-tile-content>Grado:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
+                     </v-list-tile>
+                     <v-list-tile>
+                        <v-list-tile-content>Dia:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
+                     </v-list-tile>
+                     <v-list-tile>
+                        <v-list-tile-content>Horario:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
+                     </v-list-tile>
+                     <v-list-tile>
+                        <v-list-tile-content class="align-center">
+                           <v-btn round dark color="primary" block>Seleccionar</v-btn>
+                        </v-list-tile-content>
+                     </v-list-tile>
+                  </v-list>
+               </v-card>
+            </v-flex>
+         </template>
+      </v-data-iterator>
+   </v-container>
+</template>
+
+<script>
+export default {
+   data: () => ({
+      rowsPerPageItems: [4, 8, 12],
+      pagination: {
+         rowsPerPage: 4
+      },
+      items: [
+         {
+            name: "Ciencias",
+            calories: 159,
+            fat: 6.0,
+            carbs: 24
+         },
+         {
+            name: "Matematicas",
+            calories: 237,
+            fat: 9.0,
+            carbs: 37
+         },
+         {
+            name: "Español",
+            calories: 262,
+            fat: 16.0,
+            carbs: 23
+         },
+         {
+            name: "Ingles",
+            calories: 305,
+            fat: 3.7,
+            carbs: 67
+         },
+         {
+            name: "Estudios Sociales",
+            calories: 356,
+            fat: 16.0,
+            carbs: 49
+         },
+         {
+            name: "Jelly bean",
+            calories: 375,
+            fat: 0.0,
+            carbs: 94
+         },
+         {
+            name: "Lollipop",
+            calories: 392,
+            fat: 0.2,
+            carbs: 98
+         },
+         {
+            name: "Honeycomb",
+            calories: 408,
+            fat: 3.2,
+            carbs: 87
+         },
+         {
+            name: "Donut",
+            calories: 452,
+            fat: 25.0,
+            carbs: 51
+         },
+         {
+            name: "KitKat",
+            calories: 518,
+            fat: 26.0,
+            carbs: 65
+         }
+      ]
+   })
+};
+</script>
